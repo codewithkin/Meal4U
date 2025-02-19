388f94067eb4870ac47474d711442f628f164dad
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-19 16:37:36 +0200

    Set default category on load and handle missing category error in components

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -7,11 +7,17 @@ export default function Categories({ categories }: {categories: category[]}) {
     // Get a function to update the category
     const setCategory = useCategoryStore(state => state.setCategory);
 
+    // Set the category to be the first category by default
+    setCategory(categories[0].idCategory);
+
     return (
         <article className="w-full justify-center items-center overflow-x-scroll md:overflow-x-hidden flex gap-4 items-center p-4">
             {
                 categories.map((category: category) => (
                     <Badge 
+                    onClick={() => {
+                        setCategory(category.idCategory)
+                    }}
                     color="primary"
                     key={category.idCategory}>
                         {category.strCategory}
--- src/components/CategoryMeals.tsx
+++ src/components/CategoryMeals.tsx
@@ -5,6 +5,8 @@ export default function CategoryMeals () {
     // Get the current category from the zustand store
     const category = useCategoryStore(state => state.category);
 
+    if (!category) throw new Error ("No category");
+
     return (
         <article className="grid md:grid-cols-3 lg:grid-cols-4 sm:grod-cols-2">
             <h2>Howdie !</h2>
