00d907f859c4d2a47a349682629045aa18ce941c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-19 16:36:14 +0200

    Integrate Zustand store into Categories and CategoryMeals components for state management

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,7 +1,12 @@
+"use client";
 import { category } from "@/types";
 import { Badge } from "./ui/badge";
+import useCategoryStore from "@/stores/categoryStore";
 
 export default function Categories({ categories }: {categories: category[]}) {
+    // Get a function to update the category
+    const setCategory = useCategoryStore(state => state.setCategory);
+
     return (
         <article className="w-full justify-center items-center overflow-x-scroll md:overflow-x-hidden flex gap-4 items-center p-4">
             {
--- src/components/CategoryMeals.tsx
+++ src/components/CategoryMeals.tsx
@@ -1,4 +1,10 @@
+"use client";
+import useCategoryStore from "@/stores/categoryStore"
+
 export default function CategoryMeals () {
+    // Get the current category from the zustand store
+    const category = useCategoryStore(state => state.category);
+
     return (
         <article className="grid md:grid-cols-3 lg:grid-cols-4 sm:grod-cols-2">
             <h2>Howdie !</h2>
