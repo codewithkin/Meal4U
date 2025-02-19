fc9dd2c126bdd2b82e1ef1075375854a5de605b9
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-19 16:28:06 +0200

    Refactor category fetching to use axios and simplify Page component

--- src/helpers/queries/getCategories.ts
+++ src/helpers/queries/getCategories.ts
@@ -5,6 +5,8 @@ export default async function getCategories () {
     try {
         const response = await axios.get(`${frontendUrl}/api/meals/categories`);
 
+        console.log("DATA: ", response);
+
         return response.data.categories;
     } catch (e) {
         console.log("An error occured while fetching categories: ", e);
