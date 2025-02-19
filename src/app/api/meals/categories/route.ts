7af98d9f71c3b3822e8a589af5c60ae2beb7ad86
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-19 15:55:54 +0200

    Refactor meal categories API response handling and type definitions

--- src/app/api/meals/categories/route.ts
+++ src/app/api/meals/categories/route.ts
@@ -1,16 +1,16 @@
-import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import axios from "axios";
+import { category } from "@/types";
 
 export async function GET () {
     try {
         // Get all of the meal categories
         const response = await axios.get("https://www.themealdb.com/api/json/v1/1/categories.php");
 
-        const categoryData = response.data
-        console.log("DATA: ", categoryData);
+        const categories = response.data as { categories: category[] };
+        console.log("DATA: ", categories);
 
-        return NextResponse.json(categoryData);
+        return NextResponse.json(categories);
     } catch (e) {
         console.log("An error occured while fetching categories: ", e);
         return NextResponse.json({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import frontendUrl from '@/lib/frontendUrl';
+import { category } from '@/types';
 import axios from 'axios';
 
 async function Page() {
   // Fetch all of the meal categories
-  const categories = await axios.get(`${frontendUrl}/api/meals/categories`);
+  const categories = await axios.get(`${frontendUrl}/api/meals/categories`) as { categories: category[] };
 
   console.log("All categories: ", categories);
 
