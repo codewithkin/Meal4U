c468612e2e8d140f8714593c780caea961dda908
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-19 16:23:33 +0200

    Implement category fetching with React Query and add getCategories helper function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
+"use client";
 import { Badge } from '@/components/ui/badge';
+import getCategories from '@/helpers/queries/getCategories';
 import frontendUrl from '@/lib/frontendUrl';
 import { category } from '@/types';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { Loader2 } from 'lucide-react';
 
-async function Page() {
+function Page() {
   // Fetch all of the meal categories
-  const {data: {categories: { categories }}} = await axios.get(`${frontendUrl}/api/meals/categories`) as { data: {categories: { categories: category[] }}; categories: category[] };
+  const { data: categories, isLoading, isError, error } = useQuery({
+    queryKey: ["categories"],
+    queryFn: getCategories
+  });
 
-  
+  if (isLoading) {
+    <Loader2 className="animate-spin" />
+  }
+
+  if (isError) {
+    <h2>An error occured</h2>
+  } else {
+    console.log("ALL CATEGORIES: ", categories);
+  }
 
   return (
     <article>
-        <article className='bg-slate-800 py-2 px-4'>
+        <article className='bg-slate-800 p-4'>
             <h2 className='text-green-300 text-2xl font-semibold'>Meals4U</h2>
         </article>
 
---
+++ src/helpers/queries/getCategories.ts
@@ -0,0 +1,12 @@
+import frontendUrl from "@/lib/frontendUrl";
+import axios from "axios";
+
+export default async function getCategories () {
+    try {
+        const response = await axios.get(`${frontendUrl}/api/meals/categories`);
+
+        return response.data.categories;
+    } catch (e) {
+        console.log("An error occured while fetching categories: ", e);
+    }
+}
